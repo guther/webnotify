43f63691dac9b8c08b38198926f927602a1e858c
Guther <guther.manaus@gmail.com>
2018-02-09 00:17:41 -0400

    ...

--- notificacoes.php
+++ notificacoes.php
@@ -14,39 +14,6 @@ catch(PDOException $e){
 }
 
 
-try {
-    $conn = new PDO("mysql:host=localhost;dbname=$db", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully!";
-}
-catch(PDOException $e){
-    echo "Connection failed: " . $e->getMessage();
-}
-
-
-try {
-    $conn = new PDO("mysql:host=127.0.0.1;dbname=$db", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully!";
-}
-catch(PDOException $e){
-    echo "Connection failed: " . $e->getMessage();
-}
-
-
-try {
-    $conn = new PDO("mysql:host=172.30.95.107;dbname=$db", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully!";
-}
-catch(PDOException $e){
-    echo "Connection failed: " . $e->getMessage();
-}
-
-
 
 exit;
 
