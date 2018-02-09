adb9810b9c70e2c339126aa29ce1ea252b6555ec
Guther <guther.manaus@gmail.com>
2018-02-08 21:59:48 -0400

    ...

---
+++ notificacoes.php
@@ -0,0 +1,35 @@
+<?php
+
+if(isset($_GET["id"]))
+	$id = $_GET["id"];
+else
+	$id = 0;
+
+
+$notificacoes = [];
+
+$notificacoes[1] = ["id" => 1, "notificacao" => ["icon" => "http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png", "title" => "Titulo 1", "body" => "Hey there! You've been notified!"]];
+$notificacoes[2] = ["id" => 2, "notificacao" => ["icon" => "http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png", "title" => "Titulo 2", "body" => "Notificacao 2"]];
+$notificacoes[3] = ["id" => 3, "notificacao" => ["icon" => "http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png", "title" => "Titulo 3", "body" => "Notificacao 3 !!!"]];
+$notificacoes[4] = ["id" => 4, "notificacao" => ["icon" => "http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png", "title" => "Titulo 4", "body" => "Notificacao 4 !!!"]];
+
+$json = false;
+
+if($id>0){
+	foreach($notificacoes as $key => $value){
+		if($key > $id){
+			$json = json_encode($notificacoes[$key]);
+			break;
+		}
+	}
+}
+else{
+	$first_key = key($notificacoes); // First Element's Key
+	$json = json_encode($notificacoes[$first_key]);
+}
+
+//header('Content-Type: application/json');
+
+echo $json;
+
+?>
---
+++ worker_notify.js
@@ -0,0 +1,66 @@
+self.addEventListener('message', function(e) {
+  var data = e.data;
+  switch (data.cmd) {
+    case 'start':
+      fnc_init_check_notify(data.notification_id);
+      break;
+    case 'set_time_start':
+      setTimeout(function(){fnc_init_check_notify(data.notification_id);},5000);
+      break;
+    case 'stop':
+      self.close(); // Encerra o worker
+      break;
+    default:
+      self.postMessage('Unknown command: ' + data.msg);
+  };
+}, false);
+
+function fnc_is_json_string(str) {
+    try {
+        JSON.parse(str);
+    }
+    catch (e) {
+        return false;
+    }
+    return true;
+}
+
+function fnc_init_check_notify(notification_id){
+	
+	var param = typeof(notification_id)=="number"?notification_id:0;
+	
+	var ajaxurl = 'http://localhost/notificacoes.php?id='+param;
+	var xmlHttp = ajax();
+	if(xmlHttp){
+		xmlHttp.onreadystatechange=function(){
+   			if(xmlHttp.readyState==4){
+				var json = xmlHttp.responseText;
+				if(fnc_is_json_string(json))
+					self.postMessage({'cmd':'json','json':json});
+				else
+					self.postMessage({'cmd':'re_check'});
+			}
+		};
+		xmlHttp.open("get",ajaxurl,true);
+		xmlHttp.send(null);
+	}
+}
+
+function ajax() {
+    var xmlHttp;
+    try {
+        xmlHttp = new XMLHttpRequest();
+    } catch (e) {
+        try {
+            xmlHttp = new ActiveXObject("Msxml2.XMLHTTP");
+        } catch (e) {
+            try {
+                xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+            } catch (e) {
+                alert("Seu navegador não suporta AJAX!");
+                return false;
+            }
+        }
+    }
+    return xmlHttp;
+}
