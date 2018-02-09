62734b61eb94563afee8e5de028adcd310e4bbc8
Guther <guther.manaus@gmail.com>
2018-02-08 23:16:43 -0400

    ...

--- notificacoes.php
+++ notificacoes.php
@@ -1,4 +1,20 @@
 <?php
+$servername = $_ENV["MYSQL_HOST"];;
+$username = $_ENV["MYSQL_USER"];
+$password = $_ENV["MYSQL_PWD"];
+$db = $_ENV["MYSQL_DB"];
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully!";
+}
+catch(PDOException $e){
+    echo "Connection failed: " . $e->getMessage();
+}
+
+exit;
+
 
 if(isset($_GET["id"]))
 	$id = $_GET["id"];
